896e2904ce5048e4b7edbd296653e9fdd87ee2aa
Mateusz Suplewski <matx3582@gmail.com>
2022-12-13 22:57:52 +0100

    Handled states

--- src/App.js
+++ src/App.js
@@ -1,30 +1,31 @@
-import React, { useEffect, useState } from 'react'
-import { exerciseOptions, fetchData } from './api/fetchData'
-import dataPlaceholder from './data/dataPlaceholder'
+import React from 'react'
+import useExercises from './hooks/useExercises'
+import { fetchData } from './api/fetchData'
+import { exercisesContext } from '../contexts'
 
 export const App = () => {
-  const [exercisesData, setExercisesData] = useState([])
-  const [error, setError] = useState('')
-
-  useEffect(() => {
-    (async () => {
-      try {
-        const exercises = await fetchData('https://exercisedb.p.rapidapi.com/exercises', exerciseOptions)
-        const exercisesWithDescription = exercises && exercises.map(exercise => ({ ...exercise, description: dataPlaceholder }))
-        setExercisesData(exercisesWithDescription)
-      } catch (error) {
-        setError(error.message)
-      }
-    })()
-  }, [])
+  const [exercises, isLoading, error] = useExercises(fetchData)
 
   return (
-    <div>
-      {
-        exercisesData.map((exercise) => <p key={exercise.id}>{exercise.name}</p>)
-      }
-      <p>{error}</p>
-    </div>
+    <exercisesContext.Provider value={exercises}>
+      <div>
+        {isLoading
+          ? (
+            <p>Loading...</p>
+            )
+          : error
+            ? (
+              <p>{error}</p>
+              )
+            : exercises.length === 0
+              ? (
+                <p>No exercises to display</p>
+                )
+              : (
+                  exercises.map((exercise) => <p key={exercise.id}>{exercise.name}</p>)
+                )}
+      </div>
+    </exercisesContext.Provider>
   )
 }
 
