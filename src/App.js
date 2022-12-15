985d1f345a5ac03b09f1ab3edb80d5abd1d494c9
Mateusz Suplewski <matx3582@gmail.com>
2022-12-15 18:30:41 +0100

    Added pagination & routing

--- src/App.js
+++ src/App.js
@@ -2,35 +2,34 @@ import React from 'react'
 import useExercises from './hooks/useExercises'
 import { fetchData } from './api/fetchData'
 import exercisesContext from './contexts'
-
-import Navbar from './components/Navbar'
 import ListedExercises from './components/ListedExercises'
+import { Routes, Route } from 'react-router-dom'
+import Exercise from './components/Exercise'
+import Page404 from './components/Page404'
 
 export const App = () => {
   const [exercises, isLoading, error] = useExercises(fetchData)
 
   return (
-    <exercisesContext.Provider value={exercises}>
-      <div>
-        {isLoading
-          ? (
-            <p>Loading...</p>
-            )
-          : error
-            ? (
-              <p>{error}</p>
-              )
-            : exercises.length === 0
-              ? (
-                <p>No exercises to display</p>
-                )
-              : (
-                <>
-                  <Navbar appName={'Fit Planner'}/>
-                  <ListedExercises exercises={exercises}/>
-                </>
-                )}
-      </div>
+    <exercisesContext.Provider value={{ exercises, isLoading, error }}>
+      <Routes>
+        <Route
+          path={'/'}
+          element={<ListedExercises />}
+        />
+        <Route
+          path={'/exercise/:exerciseid'}
+          element={<Exercise/>}
+        />
+        <Route
+          path={'/page/:page'}
+          element={<ListedExercises/>}
+        />
+        <Route
+          path={'*'}
+          element={<Page404 />}
+        />
+      </Routes>
     </exercisesContext.Provider>
   )
 }
--- src/components/ListedExercises.js
+++ src/components/ListedExercises.js
@@ -1,91 +1,116 @@
-import React from 'react'
-import { Typography, Card, CardActions, CardContent, CardMedia, Button, Grid, Stack, Box } from '@mui/material'
+import React, { useContext } from 'react'
 import PropTypes from 'prop-types'
+import exercisesContext from '../contexts'
+import Pagination from './Pagination'
+import { Typography, Card, CardActions, CardContent, CardMedia, Button, Grid, Stack, Box } from '@mui/material'
+import { Link } from 'react-router-dom'
 
-export const ListedExercises = ({ exercises }) => {
+export const ListedExercises = () => {
+  const { exercises, isLoading, error } = useContext(exercisesContext)
   return (
-    <Grid
-      container
-      spacing={4}
-      sx={{ mt: 2 }}
-    >
-      {exercises.map((exercise) => {
-        return (
-          <Grid
-            container
-            item
-            xs={12}
-            sm={6}
-            md={4}
-            xl={3}
-            justifyContent={'center'}
-            key={exercise.id}
-          >
-            <Grid item>
-              <Card sx={{ maxWidth: 345, m: 1 }}>
-                <CardMedia
-                  component={'img'}
-                  image={exercise.gifUrl}
-                />
-                <CardContent>
-                  <Typography
-                    gutterBottom
-                    variant={'h5'}
-                    component={'div'}
-                  >
-                    {exercise.name}
-                  </Typography>
-                  <Stack
-                    direction={'row'}
-                    spacing={2}
-                    justifyContent={'flex-end'}
-                    sx={{ mt: 0.5 }}
-                  >
-                    <Box
-                      sx={{
-                        backgroundColor: 'info.main',
-                        padding: '0.1rem 0.7rem',
-                        borderRadius: '0.3rem'
-                      }}
+    <>
+      {isLoading
+        ? (
+          <p>Loading...</p>
+          )
+        : error
+          ? (
+            <p>{error}</p>
+            )
+          : exercises.length === 0
+            ? (
+              <p>No exercises to display</p>
+              )
+            : (
+              <Grid
+                container
+                spacing={4}
+                sx={{ mt: 2 }}
+              >
+                <Pagination
+                  limit={12}
+                  path={'/page'}
+                >
+                  {exercises.map(exercise => (
+                    <Grid
+                      key={exercise.id}
+                      container
+                      item
+                      xs={12}
+                      sm={6}
+                      md={4}
+                      xl={3}
+                      justifyContent={'center'}
                     >
-                      <Typography
-                        variant={'subtitle1'}
-                        color={'ButtonShadow'}
-                      >
-                        {exercise.bodyPart}
-                      </Typography>
-                    </Box>
-                    <Box
-                      sx={{
-                        backgroundColor: 'info.main',
-                        padding: '0.1rem 0.7rem',
-                        borderRadius: '0.3rem'
-                      }}
-                    >
-                      <Typography
-                        variant={'subtitle1'}
-                        color={'ButtonShadow'}
-                      >
-                        {exercise.target}
-                      </Typography>
-                    </Box>
-                  </Stack>
-                </CardContent>
-                <CardActions>
-                  <Button
-                    sx={{ width: '100%' }}
-                    size={'medium'}
-                    variant={'outlined'}
-                  >
-                    Go to exercise
-                  </Button>
-                </CardActions>
-              </Card>
-            </Grid>
-          </Grid>
-        )
-      })}
-    </Grid>
+                      <Grid item>
+                        <Card sx={{ maxWidth: 345, m: 1 }}>
+                          <CardMedia
+                            component={'img'}
+                            image={exercise.gifUrl}
+                          />
+                          <CardContent>
+                            <Typography
+                              gutterBottom
+                              variant={'h5'}
+                              component={'div'}
+                            >
+                              {exercise.name}
+                            </Typography>
+                            <Stack
+                              direction={'row'}
+                              spacing={2}
+                              justifyContent={'flex-end'}
+                              sx={{ mt: 0.5 }}
+                            >
+                              <Box
+                                sx={{
+                                  backgroundColor: 'info.main',
+                                  padding: '0.1rem 0.7rem',
+                                  borderRadius: '0.3rem'
+                                }}
+                              >
+                                <Typography
+                                  variant={'subtitle1'}
+                                  color={'ButtonShadow'}
+                                >
+                                  {exercise.bodyPart}
+                                </Typography>
+                              </Box>
+                              <Box
+                                sx={{
+                                  backgroundColor: 'info.main',
+                                  padding: '0.1rem 0.7rem',
+                                  borderRadius: '0.3rem'
+                                }}
+                              >
+                                <Typography
+                                  variant={'subtitle1'}
+                                  color={'ButtonShadow'}
+                                >
+                                  {exercise.target}
+                                </Typography>
+                              </Box>
+                            </Stack>
+                          </CardContent>
+                          <CardActions>
+                            <Button
+                              sx={{ width: '100%' }}
+                              size={'medium'}
+                              variant={'outlined'}
+                              component={Link}
+                              to={`/exercise/${exercise.id}`}
+                            >
+                              Go to exercise
+                            </Button>
+                          </CardActions>
+                        </Card>
+                      </Grid>
+                    </Grid>
+                  ))}
+                </Pagination>
+              </Grid>
+              )}
+    </>
   )
 }
 
