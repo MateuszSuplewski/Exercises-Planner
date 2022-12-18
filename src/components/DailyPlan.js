acb20c604ef265d71f4548f0b8964780522800c2
Mateusz Suplewski <matx3582@gmail.com>
2022-12-18 15:20:40 +0100

    Added option to delete exercise from plan & change order

--- src/components/DailyPlan.js
+++ src/components/DailyPlan.js
@@ -1,11 +1,26 @@
 import React, { useState } from 'react'
 import { Box, Collapse, IconButton, Table, TableBody, TableCell, TableHead, TableRow, Typography } from '@mui/material'
+import DeleteIcon from '@mui/icons-material/Delete'
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown'
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp'
 import PropTypes from 'prop-types'
+import { useDispatch, useSelector } from 'react-redux'
+import { createActionRemove, createActionSetExercisesByDay } from '../state/trainingPlan'
+import ArrowCircleUpIcon from '@mui/icons-material/ArrowCircleUp'
+import ArrowCircleDownIcon from '@mui/icons-material/ArrowCircleDown'
+import { changeArrayOrder } from '../helpers/mainHelper'
 
 const DailyPlan = ({ day, exercises }) => {
   const [open, setOpen] = useState(false)
+  const storeDispatch = useDispatch()
+  const trainingPlanState = useSelector((state) => state.trainingPlan)
+
+  const handleOrderChange = (itemPosition, changePositionBy) => {
+    const orderedData = changeArrayOrder(trainingPlanState[day], itemPosition, itemPosition + changePositionBy)
+
+    storeDispatch(createActionSetExercisesByDay(orderedData, day))
+  }
+
   return (
     <>
       <TableRow>
@@ -39,26 +54,49 @@ const DailyPlan = ({ day, exercises }) => {
               >
                 {`Training for ${day}`}
               </Typography>
-              <Table
-                size={'small'}
-              >
+              <Table size={'small'}>
                 <TableHead>
                   <TableRow>
                     <TableCell>Exercise name</TableCell>
                     <TableCell>Sets</TableCell>
                     <TableCell align={'right'}>Weight</TableCell>
                     <TableCell align={'right'}>Repetitions</TableCell>
+                    <TableCell />
                   </TableRow>
                 </TableHead>
                 <TableBody>
-                  {exercises.map(({ name, sets, weight, repetitions }, index) => (
-                    <TableRow key={index}>
-                      <TableCell>{name}</TableCell>
-                      <TableCell>{sets}</TableCell>
-                      <TableCell align={'right'}>{weight}</TableCell>
-                      <TableCell align={'right'}>{repetitions}</TableCell>
-                    </TableRow>
-                  ))}
+                  {exercises.map(
+                    ({ name, sets, weight, repetitions, id }, index) => (
+                      <TableRow key={index}>
+                        <TableCell>{name}</TableCell>
+                        <TableCell>{sets}</TableCell>
+                        <TableCell align={'right'}>{weight}</TableCell>
+                        <TableCell align={'right'}>{repetitions}</TableCell>
+                        <TableCell>
+                          <IconButton
+                            color={'primary'}
+                            disabled={index === 0}
+                            onClick={() => handleOrderChange(index, -1)}
+                          >
+                            <ArrowCircleUpIcon />
+                          </IconButton>
+                          <IconButton
+                            color={'primary'}
+                            disabled={exercises.length - 1 === index}
+                            onClick={() => handleOrderChange(index, 1)}
+                          >
+                            <ArrowCircleDownIcon />
+                          </IconButton>
+                          <IconButton
+                            color={'error'}
+                            onClick={() => storeDispatch(createActionRemove({ name, sets, weight, repetitions, id }, day))}
+                          >
+                            <DeleteIcon />
+                          </IconButton>
+                        </TableCell>
+                      </TableRow>
+                    )
+                  )}
                 </TableBody>
               </Table>
             </Box>
