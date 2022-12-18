e2cf6e6d858988019ed292234361d4a0a1a4b82d
Mateusz Suplewski <matx3582@gmail.com>
2022-12-18 15:50:19 +0100

    Added informing message while adding new exercise

--- src/components/exerciseForm.js
+++ src/components/exerciseForm.js
@@ -1,8 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import Input from './Input'
 import Select from './Select'
-import { Stack, Button } from '@mui/material'
+import { Stack, Button, Snackbar } from '@mui/material'
 import validateForm from '../helpers/validateForm'
 import useForm from '../hooks/useForm'
 import { useDispatch } from 'react-redux'
@@ -25,6 +25,7 @@ const ExerciseForm = ({ exerciseName, fields }) => {
 
   const storeDispatch = useDispatch()
   const { values, errors, setErrors, handleInputChange, resetForm } = useForm(initialState)
+  const [openSnackbar, setOpenSnackbar] = useState(false)
 
   const handleFormSubmit = (e) => {
     e.preventDefault()
@@ -34,40 +35,48 @@ const ExerciseForm = ({ exerciseName, fields }) => {
       setErrors(foundedErrors)
       return
     }
-
+    setOpenSnackbar(true)
     const exercise = { ...values, name: exerciseName, id: uuidv4() }
     storeDispatch(createActionAdd(exercise))
     resetForm()
   }
 
   return (
-    <form
-      style={{ maxWidth: '420px', padding: '1rem' }}
-      onSubmit={handleFormSubmit}
-    >
-      <Stack spacing={2}>
-        {fields.map((field, index) => {
-          const FieldComponent = data[field.type]
-          return (
-            <FieldComponent
-              key={index}
-              {...field}
-              value={values[field.name]}
-              onChange={handleInputChange}
-              error={errors[field.name]}
-            />
-          )
-        })}
-      </Stack>
-      <Button
-        variant={'contained'}
-        sx={{ mt: 0.75 }}
-        fullWidth
-        type={'submit'}
+    <>
+      <form
+        style={{ maxWidth: '420px', padding: '1rem' }}
+        onSubmit={handleFormSubmit}
       >
-        ADD EXERCISE
-      </Button>
-    </form>
+        <Stack spacing={2}>
+          {fields.map((field, index) => {
+            const FieldComponent = data[field.type]
+            return (
+              <FieldComponent
+                key={index}
+                {...field}
+                value={values[field.name]}
+                onChange={handleInputChange}
+                error={errors[field.name]}
+              />
+            )
+          })}
+        </Stack>
+        <Button
+          variant={'contained'}
+          sx={{ mt: 0.75 }}
+          fullWidth
+          type={'submit'}
+        >
+          ADD EXERCISE
+        </Button>
+      </form>
+      <Snackbar
+        open={openSnackbar}
+        autoHideDuration={3000}
+        onClose={() => setOpenSnackbar(false)}
+        message={'Successfully added new exercise!'}
+      />
+    </>
   )
 }
 
